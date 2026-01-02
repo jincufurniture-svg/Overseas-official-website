2d1a2bd87381f238fff89da884ac271071616b5f
peal_L <674744961@qq.com>
2026-01-02 19:53:45 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,6 @@ export default defineNuxtConfig({
     key: process.env.SUPABASE_KEY || ''
   },
   build: {
-    transpile: ['pinia', 'element-plus/dist/index.full.js']
+    transpile: ['pinia', 'element-plus']
   }
 })
